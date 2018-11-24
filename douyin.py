b825ac5cf58952fbf34471df030f96d4d5700e5f
Suacrbah <1678693637@qq.com>
2018-11-24 17:41:29 +0800

    On branch master

--- douyin.py
+++ douyin.py
@@ -1,4 +1,4 @@
-import time
+import sys
 import json
 import urllib.request
 
@@ -7,11 +7,10 @@ total = count = 0
 # clear the file to store the comments
 open('comments.txt', 'w').close()
 
+del sys.argv[0]
+
 while True:
     awe_id = input("Please input the id of the video (q to quit): ")
-
-    start = time.time()
-
     if awe_id == 'q':
         break
 
@@ -22,7 +21,7 @@ while True:
     while True:
         url = ('https://aweme.snssdk.com/aweme/v2/comment/list/?cursor='
             + str(cursor) + '&aweme_id=' + awe_id + '&count=50')
-        ##print(url)
+        print(url)
         cursor += 50
 
         # open the url
@@ -31,27 +30,31 @@ while True:
         # load the data in format json
         data = json.load(http_res)
 
-        # write to the file
-        ##with open('comments.txt', 'a+') as fobj:
-        for d in data['comments']:
-            sub_total += 1
-            total += 1
+        #write to the file
+        with open('comments.txt', 'a+') as fobj:
+            for d in data['comments']:
+                sub_total += 1
+                total += 1
+                flag = 0
 
-            # the comments contain the following words
-            if ('美' in d['text'] or '漂亮' in d['text'] or '好看' in d['text'] or '可爱' in d['text']):
-                    ##print(sub_total)
-                count += 1
+                # check if the comments contain the desired words
+                for arg in sys.argv:
+                    if arg in d['text']:
+                        flag = 1
+
+                # the comments contain the desired words
+                if flag:
+                    print(sub_total)
+                    count += 1
 
                 # write to file in desired format: index text like_number
-                ##fobj.write(str(sub_total) + ' ' + d['text'])
-                ##fobj.write(' ' + str(d['digg_count']) + '\n')
+                fobj.write(str(sub_total) + ' ' + d['text'])
+                fobj.write(' ' + str(d['digg_count']) + '\n')
 
             # if there is no comment anymore
-        if (data['has_more'] == 0 or total >= 200):
-            ##fobj.write('\n')
-            break
-
-    print('--- %s seconds ---' % (time.time() - start))
+            if (data['has_more'] == 0):
+                fobj.write('\n')
+                break
 
 try:
     print('Analyzed total of ' + str(total) + ' comments')
